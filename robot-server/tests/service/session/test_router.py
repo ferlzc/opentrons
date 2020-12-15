53d2305f8eecdeea69293176f18c643578a850a2
Laura Cox <lauracox@Lauras-MacBook-Pro.local>
2020-12-15 10:23:22 -0500

    Fixup router test

--- robot-server/tests/service/session/test_router.py
+++ robot-server/tests/service/session/test_router.py
@@ -365,13 +365,13 @@ def test_execute_command_no_body(api_client,
     """Test that a command with empty body can be accepted"""
     response = api_client.post(
         f"/sessions/{mock_session_meta.identifier}/commands/execute",
-        json=command("calibration.loadLabware", None)
+        json=command("calibration.moveToTipRack", None)
     )
 
     mock_command_executor.execute.assert_called_once_with(
         Command(
             content=CommandContent(
-                name=CalibrationCommand.load_labware,
+                name=CalibrationCommand.move_to_tip_rack,
                 data=EmptyModel()),
             meta=CommandMeta(command_id, command_created_at)
         )
@@ -379,7 +379,7 @@ def test_execute_command_no_body(api_client,
 
     assert response.json() == {
         'data': {
-            'command': 'calibration.loadLabware',
+            'command': 'calibration.moveToTipRack',
             'data': {},
             'status': 'executed',
             'createdAt': '2000-01-01T00:00:00',
