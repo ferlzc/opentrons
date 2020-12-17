19b2b1b733f7d760a2cfe061753d99bf74422418
Seth Foster <seth@opentrons.com>
2020-12-17 12:47:13 -0500

    make some const definitions more cohesive

--- api/src/opentrons/drivers/smoothie_drivers/driver_3_0.py
+++ api/src/opentrons/drivers/smoothie_drivers/driver_3_0.py
@@ -5,7 +5,6 @@ import logging
 from time import sleep, time
 from threading import Event, RLock
 from typing import Any, Dict, Optional, Union, List, Tuple
-from typing_extensions import Final
 
 from math import isclose
 from serial.serialutil import SerialException  # type: ignore
@@ -16,6 +15,9 @@ from opentrons.drivers.utils import (
     AxisMoveTimestamp, parse_key_from_substring, parse_number_from_substring)
 from opentrons.drivers.rpi_drivers.gpio_simulator import SimulatingGPIOCharDev
 from opentrons.system import smoothie_update
+from . import HOMED_POSITION, Y_BOUND_OVERRIDE
+
+
 """
 - Driver is responsible for providing an interface for motion control
 - Driver is the only system component that knows about GCODES or how smoothie
@@ -29,17 +31,6 @@ log = logging.getLogger(__name__)
 
 ERROR_KEYWORD = 'error'
 ALARM_KEYWORD = 'alarm'
-
-# TODO (artyom, ben 20171026): move to config
-HOMED_POSITION: Final = {
-    'X': 418.0,
-    'Y': 353.0,
-    'Z': 218.0,
-    'A': 218.0,
-    'B': 19.0,
-    'C': 19.0
-}
-Y_BOUND_OVERRIDE: Final = 370
 
 
 PLUNGER_BACKLASH_MM = 0.3
